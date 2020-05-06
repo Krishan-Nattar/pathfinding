b435301eedd2f6a9b4d72c56e38897d5dc9f8811
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-06 11:29:28 -0700

    starting and ending nodes can be selected

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -4,18 +4,55 @@ import './Node.css'
 
 const Node = (props) => {
     const handleMouseDown = (e) =>{
-        console.log(e.target);
-        let node = e.target;
-        node.classList.toggle('toggler')
-        props.setMouseDown(true);
+        // console.log(e.target);
+        if(!props.selectStartNode && !props.selectEndNode){
+            let node = e.target;
+            if(!node.classList.contains('start-node')){
+                node.classList.toggle('toggler')
+                props.setMouseDown(true);
+            }
+        } else if(props.selectStartNode) {
+
+            let node = e.target;
+            if(node.classList.contains('end-node')){
+                return
+            }
+
+
+
+
+
+            const nodeList = document.querySelectorAll('.node');
+            for(const node of nodeList)
+            if (node.classList.contains('start-node')){
+                node.classList.toggle('start-node')
+            }
+
+            node.classList.toggle('start-node');
+            props.setSelectStartNode(false)
+        } else if(props.selectEndNode){
+            let node = e.target;
+            if(node.classList.contains('start-node')){
+                return
+            }
+
+            const nodeList = document.querySelectorAll('.node');
+            for(const node of nodeList)
+            if (node.classList.contains('end-node')){
+                node.classList.toggle('end-node')
+            }
+
+            node.classList.toggle('end-node');
+            props.setSelectEndNode(false)
+        }
     }
 
 
     const handleDrag = e =>{
         if(props.mouseDown === true){
-            console.log('dragging')
+            // console.log('dragging')
             let node = e.target;
-            if(!node.classList.contains('toggler')){
+            if(!node.classList.contains('toggler') && !node.classList.contains('start-node') && !node.classList.contains('end-node')){
 
                 node.classList.toggle('toggler');
             }
@@ -23,7 +60,6 @@ const Node = (props) => {
     }
     return ( 
         <div className="node" onMouseDown={handleMouseDown} onMouseOver={handleDrag}>
-            {/* Node */}
         </div>
      );
 }
--- src/components/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer.js
@@ -1,54 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Node from './Node/Node';
 import './PathfindingVisualizer.css';
 
-
 const PathfindingVisualizer = (props) => {
+	const [nodes, setNodes] = useState([]);
+	const [selectStartNode, setSelectStartNode] = useState(false);
+	const [selectEndNode, setSelectEndNode] = useState(false);
 
-    const [nodes, setNodes] = useState([])
+	const handleClearSelected = () => {
+		const nodeList = document.querySelectorAll('.node');
+		for (const node of nodeList) node.className = 'node';
+		// if (node.classList.contains('toggler')){
+		//     node.classList.toggle('toggler')
+		// }
+	};
 
-    const handleClearSelected = () =>{
-        const nodeList = document.querySelectorAll('.node');
-        for(const node of nodeList)
-        if (node.classList.contains('toggler')){
-            node.classList.toggle('toggler')
-        }
-    }
+	const handleSelectStartingNode = (e) => {
+        setSelectStartNode(true);
+        setSelectEndNode(false);
+		// console.log('hi')
+	};
 
-    const handleSelectStartingNode = e =>{
-        console.log('hi')
-    }
-    
+	const handleSelectEndingNode = (e) => {
+		// console.log('hi')
+		setSelectStartNode(false);
+		setSelectEndNode(true);
+	};
 
-    useEffect(()=>{
-        console.log('hi')
-        const nodes = []
-        for(let i = 0; i < 10; i++){
-            const currentRow = [];
-            for(let j = 0; j < 20; j++){
-                currentRow.push([]);
-            }
-            nodes.push(currentRow);
-        }
-        setNodes(nodes);
-    },[]);
+	useEffect(() => {
+		// console.log('hi')
+		const nodes = [];
+		for (let i = 0; i < 10; i++) {
+			const currentRow = [];
+			for (let j = 0; j < 20; j++) {
+				currentRow.push([]);
+			}
+			nodes.push(currentRow);
+		}
+		setNodes(nodes);
+	}, []);
 
-    return ( 
+	return (
+		<div className={`grid ${selectStartNode || selectEndNode ? 'selecting-node' : null}`}>
+			{nodes.map((row, rowIndex) => {
+				return (
+					<div key={rowIndex} className="row">
+						{row.map((node, nodeIndex) => {
+							return (
+								<Node
+									key={nodeIndex}
+									mouseDown={props.mouseDown}
+									setMouseDown={props.setMouseDown}
+									selectStartNode={selectStartNode}
+                                    setSelectStartNode={setSelectStartNode}
+                                    setSelectEndNode={setSelectEndNode}
+                                    selectEndNode={selectEndNode}
 
-        <div className="grid">
-            {nodes.map((row, rowIndex)=>{
-                return (
-                <div key = {rowIndex} className="row">
-                    {row.map((node, nodeIndex)=>{
-                        return <Node key={nodeIndex} mouseDown={props.mouseDown} setMouseDown={props.setMouseDown} />
-                    })}
-                </div>
-                )
-            })}
-            <button onClick={handleClearSelected}>Clear Selected</button>
-            <button onClick={handleSelectStartingNode}>Select Starting Node</button>
-        </div>
-     );
-}
- 
+								/>
+							);
+						})}
+					</div>
+				);
+			})}
+			<button onClick={handleClearSelected}>Clear Selected</button>
+			<button onClick={handleSelectStartingNode}>Select Starting Node</button>
+			<button onClick={handleSelectEndingNode}>Select Ending Node</button>
+		</div>
+	);
+};
+
 export default PathfindingVisualizer;
