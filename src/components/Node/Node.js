c53901c37123175e3b74b7ce281c9aa5c25496bb
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-05 23:08:01 -0700

    styling grids

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,10 +1,19 @@
 import React, {useState, useEffect} from 'react';
+import './Node.css'
 
 
 const Node = () => {
+    const handleNodeClick = (e) =>{
+        console.log(e.target);
+        let node = e.target;
+        node.classList.toggle('toggler')
+    }
+    const handleDrag = e =>{
+        console.log('dragging')
+    }
     return ( 
-        <div>
-            Node
+        <div className="node" onClick={handleNodeClick} >
+            {/* Node */}
         </div>
      );
 }
--- src/components/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Node from './Node/Node';
+import './PathfindingVisualizer.css';
 
 
 const PathfindingVisualizer = () => {
@@ -9,19 +10,29 @@ const PathfindingVisualizer = () => {
     useEffect(()=>{
         console.log('hi')
         const nodes = []
-        for(let i = 0; i < 15; i++){
+        for(let i = 0; i < 10; i++){
             const currentRow = [];
-            for(let j = 0; j < 50; j++){
+            for(let j = 0; j < 20; j++){
                 currentRow.push([]);
             }
             nodes.push(currentRow);
         }
+        setNodes(nodes);
     },[]);
 
     return ( 
         <div>
             PathfindingVisualizer
-            <Node />
+            {nodes.map((row, rowIndex)=>{
+                return (
+                <div key = {rowIndex} className="row">
+                    {row.map((node, nodeIndex)=>{
+                        return <Node key={nodeIndex} />
+                    })}
+                </div>
+                )
+            })}
+            {/* <Node /> */}
         </div>
      );
 }
