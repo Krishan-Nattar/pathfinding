2ec104f9ed018986f9df7a52e558790afcde81a7
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-05 22:25:28 -0700

    setting up node state

--- src/components/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Node from './Node/Node';
 
 
 const PathfindingVisualizer = () => {
+
+    const [nodes, setNodes] = useState([])
+
+    useEffect(()=>{
+        console.log('hi')
+        const nodes = []
+        for(let i = 0; i < 15; i++){
+            const currentRow = [];
+            for(let j = 0; j < 50; j++){
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+    },[]);
+
     return ( 
         <div>
             PathfindingVisualizer
