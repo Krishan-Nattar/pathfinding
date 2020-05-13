cbe68c1a9927f4eea313e3085b9aa618d4401da8
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-12 21:19:52 -0700

    added color to intro explanations

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -20,13 +20,11 @@ const Modals = () => {
 		setOpen2(true);
 	};
 	const nextModal2 = () => {
-		// setOpen(false);
 		setOpen2(false);
 		setOpen3(true);
 	};
 
 	const nextModal3 = () => {
-		// setOpen(false);
 		setOpen3(false);
 		setOpen4(true);
 	};
@@ -36,18 +34,14 @@ const Modals = () => {
 			<Modal dimmer={true} open={open} onClose={close}>
 				<Modal.Header>Welcome!</Modal.Header>
 				<Modal.Content image>
-					{/* <Image
-              wrapped
-              size='medium'
-              src='/images/avatar/large/rachel.png'
-            /> */}
 					<Modal.Description>
 						<Header>Welcome to my path visualization app!</Header>
 						<p>
-							If you want to jump right in, just click skip, otherwise click
-							next for a walkthrough of how this works!
+							If you want to jump right in, just click <strong>skip</strong>;
+							otherwise click
+							<strong style={{ color: 'green' }}> next</strong> for a walkthrough
+							of how this works!
 						</p>
-						{/* <p>Is it okay to use this photo?</p> */}
 					</Modal.Description>
 				</Modal.Content>
 				<Modal.Actions>
@@ -64,20 +58,14 @@ const Modals = () => {
 				</Modal.Actions>
 			</Modal>
 
-{/* second modal */}
+			{/* second modal */}
 			<Modal dimmer={true} open={open2} onClose={close}>
 				<Modal.Header>What does this do?</Modal.Header>
 				<Modal.Content image>
-					<Image
-              wrapped
-              size='medium'
-              src='/modal1.PNG'
-            />
+					<Image wrapped size="medium" src="/modal1.PNG" />
 					<Modal.Description>
-						{/* <Header>Here is an explanation</Header> */}
 						<br />
 						<div className="node-arrows">
-							{/* <div> */}
 							<Popup
 								content="The search algorithm will start from this node."
 								trigger={<div className="node legend-start"></div>}
@@ -99,8 +87,9 @@ const Modals = () => {
 						</div>
 						<br />
 						<p>
-							After you place a starting node and an ending node you get to
-							see a visualization of how a search algorithm finds its way between them!
+							After you place a <strong style={{color: 'lightsalmon'}}>starting node</strong> and an <strong style={{color: 'violet'}}>ending node</strong> you get to see
+							a visualization of how a search algorithm finds its way between
+							them!
 						</p>
 					</Modal.Description>
 				</Modal.Content>
@@ -117,20 +106,15 @@ const Modals = () => {
 					/>
 				</Modal.Actions>
 			</Modal>
+
 			{/* Third Modal */}
 			<Modal dimmer={true} open={open3} onClose={close}>
 				<Modal.Header>Placing Walls</Modal.Header>
 				<Modal.Content image>
-					<Image
-              wrapped
-              size='medium'
-              src='/modal2.PNG'
-            />
+					<Image wrapped size="medium" src="/modal2.PNG" />
 					<Modal.Description>
-						{/* <Header>Here is an explanation</Header> */}
 						<br />
 						<div className="node-arrows">
-							{/* <div> */}
 							<Popup
 								content="The search algorithm will start from this node."
 								trigger={<div className="node legend-start"></div>}
@@ -141,7 +125,9 @@ const Modals = () => {
 							<Icon className="arrow right" color="purple" />
 							<Popup
 								content="Click anywhere to place walls and see what happens!"
-								trigger={<div className="node legend-block row-one-block"></div>}
+								trigger={
+									<div className="node legend-block row-one-block"></div>
+								}
 							/>
 							<Popup
 								content="Click anywhere to place walls and see what happens!"
@@ -152,27 +138,25 @@ const Modals = () => {
 								trigger={<div className="node legend-block"></div>}
 							/>
 							<Icon className="arrow right row-one-arrows" color="purple" />
-							{/* <Icon className="arrow right" color="purple" /> */}
 
 							<Popup
 								content="This is the final destination the algorithm is looking for."
 								trigger={<div className="node legend-end"></div>}
 							/>
 						</div>
-						<div className="node-arrows row-two" >
-						
+						<div className="node-arrows row-two">
 							<Icon className="arrow down" color="purple" />
 							<Icon className="arrow right" color="purple" />
 							<Icon className="arrow right" color="purple" />
 							<Icon className="arrow right" color="purple" />
 							<Icon className="arrow right" color="purple" />
-						
+
 							<Icon className="arrow up" color="purple" />
-						
 						</div>
 						<br />
 						<p>
-							Click/drag to add walls and watch how the algorithm navigates around them
+							Click/drag to add <strong style={{color: 'rgb(122, 168, 43)'}}>walls</strong> and watch how the algorithm navigates
+							around them
 						</p>
 					</Modal.Description>
 				</Modal.Content>
@@ -194,58 +178,10 @@ const Modals = () => {
 			<Modal dimmer={true} open={open4} onClose={close}>
 				<Modal.Header>Choose an algorithm</Modal.Header>
 				<Modal.Content image>
-					{/* <Image
-              wrapped
-              size='medium'
-              src='/modal2.PNG'
-            /> */}
 					<Modal.Description>
-						{/* <Header>Here is an explanation</Header> */}
-						{/* <br /> */}
-						{/* <div className="node-arrows"> */}
-							{/* <div> */}
-							{/* <Popup
-								content="The search algorithm will start from this node."
-								trigger={<div className="node legend-start"></div>}
-							/>
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Popup
-								content="Click anywhere to place walls and see what happens!"
-								trigger={<div className="node legend-block row-one-block"></div>}
-							/>
-							<Popup
-								content="Click anywhere to place walls and see what happens!"
-								trigger={<div className="node legend-block"></div>}
-							/>
-							<Popup
-								content="Click anywhere to place walls and see what happens!"
-								trigger={<div className="node legend-block"></div>}
-							/>
-							<Icon className="arrow right row-one-arrows" color="purple" /> */}
-							{/* <Icon className="arrow right" color="purple" /> */}
-
-							{/* <Popup
-								content="This is the final destination the algorithm is looking for."
-								trigger={<div className="node legend-end"></div>}
-							/>
-						</div>
-						<div className="node-arrows row-two" >
-						
-							<Icon className="arrow down" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-							<Icon className="arrow right" color="purple" />
-						
-							<Icon className="arrow up" color="purple" />
-						
-						</div> */}
-						{/* <br /> */}
 						<p>
-							Click the dropdown menu and choose between Breadth First Search and Depth First Search. Click begin and watch what happens!
+							Click the dropdown menu and choose between <strong>Breadth First Search </strong>
+							and <strong>Depth First Search</strong>. Click <strong style={{color: 'green'}}>begin</strong> and watch what happens!
 						</p>
 					</Modal.Description>
 				</Modal.Content>
@@ -253,13 +189,6 @@ const Modals = () => {
 					<Button color="green" onClick={close}>
 						Let's Go!
 					</Button>
-					{/* <Button
-						positive
-						icon="angle right"
-						labelPosition="right"
-						content="Next"
-						onClick={nextModal4}
-					/> */}
 				</Modal.Actions>
 			</Modal>
 		</>
